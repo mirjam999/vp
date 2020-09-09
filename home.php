6885026d2bb86cdec8e90ad0ca74a09251d452f0
Mirjam Laanejõe <mirjam999@users.noreply.github.com>
2020-09-09 15:11:33 +0300

    teise tunni failid

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Mirjam</title>
+
+</head>
+<body>
+  <h1>Mirjam Laanejõe</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p> Leht on loodud veebiproge kursuse raames <a href="http://www.tlu.ee">Tallinna Ülikoolis </a>.</p>
+
+</body>
+</html>
